b0ce00385e9dd5cdb435e086b95a6285cff2234c
github-usr786 <3221rudra@gmail.com>
2021-05-24 23:38:04 +0530

    display post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,28 @@
+const Post  = require('../models/post');
+
 // created exportable home function  (controller/callback function)
 module.exports.home  = function(request, response){
     //response.end("<h1> Express is up for codial! </h1>");
     //console.log(request.cookies);                          //reading cookies
     //response.cookie('user_id','25');                       //postig cookie to server also saves at browser
-    response.render('home',{title:"home"});
+    
+    // Post.find({}, function(err, posts){                     //all documents(entries) of collection post is fetched
+    //     if(err){console.log("Error in fetching all posts from db"); return;}
+
+        // posts has all fetched post passed to posts of home.ejs for display
+    //     response.render('home',{title:"home",posts: posts});             
+    // });
+
+//all documents(entries) of collection post is fetched 
+        //populate user of each post, fetching all details of user and extracting user.name in home.ejs
+    Post.find({}).populate('user').exec(function(err, posts){
+
+        if(err){console.log("Error in fetching all posts from db"); return;}
+        
+        // posts has all fetched post passed to posts of home.ejs for display
+        response.render('home',{title:"home",posts: posts});
+    });
+    
 }
 
 module.exports.sample  = function(request, response){
